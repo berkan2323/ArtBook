b8210f9feb529fff22b4f90d1c5df6cc43a038a5
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-02-04 13:58:23 +0300

    Add files via upload

---
+++ ArtBookProject/ArtBookProjectTests/ArtBookProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBookProjectTests.swift
+//  ArtBookProjectTests
+//
+//  Created by Berkan Aydın on 24.12.2024.
+//
+
+import Testing
+@testable import ArtBookProject
+
+struct ArtBookProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
